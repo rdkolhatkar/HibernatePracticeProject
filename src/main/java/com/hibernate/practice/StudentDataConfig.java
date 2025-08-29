c433e0e2843d193e3ae7a5db7253cbcf0a2d04b6
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-08-29 22:39:26 +0530

    Hibernate Configuration

---
+++ src/main/java/com/hibernate/practice/StudentDataConfig.java
@@ -0,0 +1,35 @@
+package com.hibernate.practice;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+
+public class StudentDataConfig {
+    @Test
+    public void testHibernateSessionConfiguration() {
+        System.out.println("Test method executed");
+
+        StudentDetails student = new StudentDetails();
+        student.setRollNo(1);
+        student.setName("John Doe");
+        student.setAge("20");
+
+        System.out.println("Student Roll No: " + student.getRollNo());
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Age: " + student.getAge());
+
+        // Now we have to create a table in the database to store this data using Hibernate
+        Configuration configuration = new Configuration(); // This is a Hibernate class which is required for session factory and it is common for all the databases
+        // Now we have to create a session factory to get the session object
+        SessionFactory sessionFactory = null; // This is an interface from Hibernate framework
+        // Now we have to configure the configuration object with the session factory
+        sessionFactory = configuration.buildSessionFactory();
+        Session session = null; // This is an interface from Hibernate framework
+        // To open a session we need to call the openSession() method of the session factory interface which returns the session object
+        session = sessionFactory.openSession();
+        // SessionFactory is an heavy object so we need to create it only once in the application
+    }
+
+}
---
+++ src/main/java/com/hibernate/practice/StudentDetails.java
@@ -0,0 +1,32 @@
+package com.hibernate.practice;
+
+public class StudentDetails {
+    // POJO class - Plain Old Java Object
+    private int rollNo;
+    private String name;
+    private String age;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
